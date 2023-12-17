76de2aca4be185747ea24ce11305cc52cdd909ee
ashutoshbr <ashutoshbr@proton.me>
2023-12-17 22:08:21 +0545

    Major refactor 💥

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def normalize_column(col) -> None:
     df["Normalized" + " " + col] = df[col].div(max_value)
 
 
-# The first container with two metrics and a line chart
+# Metrics and a line chart for rented bike count
 container1 = st.container()
 col11, col12 = container1.columns([0.15, 0.85])
 # Per day grouping
@@ -32,12 +32,11 @@ with col11:
     st.metric(
         label="Total Bikes Rented", value=millify(total_rented_bikes, precision=2)
     )
-    st.metric(label="Hours Ridden", value=millify(hours_ridden, precision=2))
     st.metric(label="Max Bikes Rented Per Day", value=grp["Rented Bike Count"].max())
 with col12:
     st.line_chart(grp, x="Date", y="Rented Bike Count")
 
-# The second container with a bar chart & a pie chart
+# Bar chart & a pie chart for Seasons
 container2 = st.container(border=True)
 col21, col22 = container2.columns([0.6, 0.4])
 with col21:
@@ -55,7 +54,7 @@ with col22:
     )
     st.altair_chart(ac)
 
-# The third container with a scatter plot
+# Scatter plot for rainfall and snowfall in normalized form
 container3 = st.container(border=True)
 with container3:
     normalize_column("Rainfall(mm)")
@@ -68,12 +67,17 @@ with container3:
         size=bubble_size,
         color=["#348abd", "#e24a33"],
     )
+# Scatter plot for rainfall only
+container3_1 = st.container()
+with container3_1:
+    st.scatter_chart(df, x="Rainfall(mm)", y="Rented Bike Count")
 
-# Fourth container with a bar chart and a scatter plot
+# Bar chart representing distribution according to holiday
 container4 = st.container(border=True)
 col41, col42, col43 = container4.columns(3)
 with col41:
     st.bar_chart(df, x="Holiday", y="Rented Bike Count")
+# Scatter plot: Wind speed & Solar Radiation distribution
 with col42:
     st.scatter_chart(
         df,
@@ -87,12 +91,18 @@ with col43:
         y="Rented Bike Count",
     )
 
-# Bubble plot
+# Bubble plot represting 3 attributes
 container5 = st.container(border=True)
 with container5:
     ac = (
         alt.Chart(df, height=600)
         .mark_point()
-        .encode(x="Temperature(°C)", y="Rented Bike Count", size="Hour")
+        .encode(x="Humidity(%)", y="Visibility (10m)", size="Solar Radiation (MJ/m2)")
     )
     st.altair_chart(ac, use_container_width=True)
+
+# Group by hour and sum
+grp_by_hour = df.groupby("Hour")["Rented Bike Count"].sum().reset_index()
+container6 = st.container()
+with container6:
+    st.line_chart(grp_by_hour, x="Hour", y="Rented Bike Count")
